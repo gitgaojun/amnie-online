4e05a723f287ea7484390141f322455217a01544
gitgaojun <1148743058@qq.com>
2017-08-06 23:10:10 +0800

    分类页面完成

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -499,7 +499,7 @@ class ControllerProductCategory extends Controller {
 		}
 
 		$category_info = $this->model_catalog_category->getCategory($category_id);
-
+//var_dump($category_info);exit;
 		if ($category_info) {
 			$this->document->setTitle($category_info['meta_title']);
 			$this->document->setDescription($category_info['meta_description']);
